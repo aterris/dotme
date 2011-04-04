afb7f771ef77be6077a22b1a5b54eb446fc2f24f
Andrew Terris <atterris@gmail.com>
2011-04-03 20:42:49 -0500

    css styling of profile

--- dotme.rb
+++ dotme.rb
@@ -14,7 +14,7 @@ get '/' do
   @name = "Andrew Terris"
   @subheading = "I love coding"
   @avatar = "http://www.gravatar.com/avatar/df5702df61888046b35a4313fc4d4cdb?s=128"
-  @bio_title = "About Me"
+  @bio_heading = "About Me"
   @bio = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
   erb :profile
