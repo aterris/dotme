b0ce9c993ed04d5ec32e97a0685fa615772bc69f
Andrew Terris <atterris@gmail.com>
2011-04-03 21:32:02 -0500

    updating names of vars

--- dotme.rb
+++ dotme.rb
@@ -9,12 +9,11 @@ end
 
 # Profile Page
 get '/' do
-  @title = "dotme"
+  @title = "Andrew Terris"
   @email = "atterris@gmail.com"
-  @name = "Andrew Terris"
-  @subheading = "I love coding"
+  @subtitle = "I love coding"
   @avatar = "http://www.gravatar.com/avatar/df5702df61888046b35a4313fc4d4cdb?s=128"
-  @bio_heading = "About Me"
+  @bio_title = "About Me"
   @bio = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
   erb :profile
