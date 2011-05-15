cc751781305acdbd2016f0437d52aeb38f5a426b
Eric Marden <eric@xentek.net>
2011-05-14 23:30:26 -0500

    MOD: dynamic-itize the profile fields

--- dotme.rb
+++ dotme.rb
@@ -4,10 +4,10 @@ set :session_secret, ENV['DOTME_SECRET']
 
 require 'sinatra/session'
 require 'models'
+require 'digest/md5'
 
-# Set Content Type
 before do 
-  headers "Content-Type" => "text/html; charset=utf-8"
+  headers "Content-Type" => "text/html; charset=utf-8" # Set Content Type
 end
 
 # Profile Page
@@ -20,26 +20,11 @@ get '/' do
        l = Link.first_or_create(:link_name => link)
      end
   else
-    @profile = Profile.all
+    @profile = Profile.get(1)
+    set :email_digest = Digest::MD5.hexdigest(@profile.email)
+    @avatar = "http://www.gravatar.com/avatar/#{email_digest}?s=175&d=retro"
     @links = Link.all
   end
-
-  @title = "Andrew Terris"
-  @email = "atterris@gmail.com"
-  @subtitle = "I love coding"
-  @avatar = "http://www.gravatar.com/avatar/df5702df61888046b35a4313fc4d4cdb?s=175"
-  @bio_title = "About Me"
-  @bio = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-  @twitter_url = "http://twitter.com/aterris"
-  @github_url = "http://github.com/aterris"
-  @linkedin_url = "http://www.linkedin.com/in/aterris"
-  @forrst_url = "http://forrst.me/aterris"
-  @dribble_url = "http://dribble.com"
-  @foursquare_url = "http://foursquare.com/aterris"
-  @gowalla_url ="http://gowalla.com/users/aterris"
-  @facebook_url = "http://www.facebook.com/atterris"
-  @blog_url = "http://wordpress.com"
   erb :index
 end
 
