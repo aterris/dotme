0a36ff3722d500422f271fd161d3ed63a0a0f0f6
Andrew Terris <atterris@gmail.com>
2011-05-20 16:51:46 -0500

    setting up click toshow instead of hover to show

--- public/js/configbar.js
+++ public/js/configbar.js
@@ -1,4 +1,12 @@
-// Hide Config Bar
+// Config Bar
 jQuery(document).ready(function($){
+	//Config Bar Control
+	$('#config-bar ul > li a').click(function(){
+		$(this).next().toggle();
+		return false;
+	});
+	
+	
+	// Hide Config Bar
 	$('#hide').click(function(){$('#config-bar').hide();return false;});
 });
