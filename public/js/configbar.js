25a74af96c28cba5c2463bd6f33e48435b2b7e8b
Andrew Terris <atterris@gmail.com>
2011-06-19 11:54:44 -0500

    cleaning up some css rules

--- public/js/configbar.js
+++ public/js/configbar.js
@@ -2,6 +2,7 @@
 jQuery(document).ready(function($){
 	//Config Bar Control
 	$('#config-nav > li a').click(function(){
+		alert('boom');
 		$('.config-bar-menu').is(':visible').hide();
 		$(this).next().toggle();
 		return false;
